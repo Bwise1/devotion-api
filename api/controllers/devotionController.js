eb4579477cd743aa18e8ec606989c588e4a39707
Bwise1 <oguntoyebenjamin2@gmail.com>
2021-11-23 17:09:05 +0200

    mongodb connections

--- api/controllers/devotionController.js
+++ api/controllers/devotionController.js
@@ -25,6 +25,24 @@ exports.read_a_devotion = function(req, res) {
     Devotion.findById(req.params.devotionId, function(err, devotion){
         if (err)
             res.send(err);
-        res.json(task);
+        res.json(devotion);
+    });
+};
+
+exports.update_a_devotion = function(req, res) {
+    Devotion.findOneAndUpdate({_id: req.params.devotionId}, req.body, {new: true}, function(err, devotion){
+        if(err)
+            res.send(err);
+        res.json(devotion);
+    });
+};
+
+exports.delete_a_devotion = function(req, res){
+    Devotion.remove({
+        _id: req.params.devotionId
+    }, function(err, devotion){
+        if(err)
+            res.send(err);
+        res.json({message: 'Devotion deleted successfully'});
     });
 };
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 8000;
+    port = process.env.PORT || 8000,
+    mongoose = require('mongoose'),
+    Devotion = require('./api/models/devotionModel'),
+    bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://libradish:ndiaboskibahoshe@ciucf-devotion.huthz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/devotionRoutes');
+routes(app);
 app.listen(port);
 console.log('ciucf devotional RESTful API server started on: '+ port);
