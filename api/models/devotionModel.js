5d2d965012bcb13261ea84e743878ff48d328456
Bwise1 <oguntoyebenjamin2@gmail.com>
2022-09-05 13:44:48 +0300

    Changed the date data type to string

--- api/models/devotionModel.js
+++ api/models/devotionModel.js
@@ -1,38 +1,35 @@
-'use strict';
-var mongoose = require('mongoose');
+"use strict";
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var DevotionSchema = new Schema({
-    topic: {
-        type: String,
-        required: 'kindly enter the Devotion title'
-    },
-    date: {
-        type: Date,
-        default: Date.now
-        //required: "kindly enter date"
-    },
-    bibleVerse: {
-        type: String,
-        required: 'kindly enter the date for the devotion'
-
-    },
-    description: {
-        type: String,
-        required: 'kindly enter the Devotion description'
-    },
-    memoryVerse: {
-        type: String,
-        required: 'kindly enter the memory verse'
-    },
-    prayerPoint: {
-        type: String,
-        required: 'kindly enter the prayer point'
-    },
-    meditate: {
-        type: String,
-        required: 'kindly enter the meditation'
-    },
-
+  topic: {
+    type: String,
+    required: "kindly enter the Devotion title",
+  },
+  date: {
+    type: String,
+    required: "kindly enter date",
+  },
+  bibleVerse: {
+    type: String,
+    required: "kindly enter the date for the devotion",
+  },
+  description: {
+    type: String,
+    required: "kindly enter the Devotion description",
+  },
+  memoryVerse: {
+    type: String,
+    required: "kindly enter the memory verse",
+  },
+  prayerPoint: {
+    type: String,
+    required: "kindly enter the prayer point",
+  },
+  meditate: {
+    type: String,
+    required: "kindly enter the meditation",
+  },
 });
-module.exports = mongoose.model('devotions', DevotionSchema);
+module.exports = mongoose.model("devotions", DevotionSchema);
